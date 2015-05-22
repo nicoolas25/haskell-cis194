f8d4457cfcd375fd158f47fd83a6d99ce4a18c6b
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-05-22 10:26:45 +0200

    Homework 1 - Exercise 2

--- Homework1.hs
+++ Homework1.hs
@@ -9,3 +9,9 @@ toDigitsRev n = right : (toDigitsRev remaining)
   where right = n `mod` 10
         remaining = n `div` 10
 
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther l = [ if i `mod` 2 == 0 then 2 * item else item | (item, i) <- lWithIndex ]
+  where lWithIndex = zip l indexes
+        indexes = [startFrom..]
+        startFrom = (length l) `mod` 2
+
