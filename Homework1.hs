290c325e1c0678b7cfdb54296046b543e39aef9c
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-05-22 13:23:19 +0200

    Homework 1 - Exercise 6

--- Homework1.hs
+++ Homework1.hs
@@ -31,8 +31,15 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi 0 _ _ _ = []
 hanoi 1 a b c = [(a, b)]
 hanoi n a b c = step1 ++ [(a, b)] ++ step3
   where step1 = hanoi (n-1) a c b
         step3 = hanoi (n-1) c b a
+
+fsa :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+fsa 0 a b c d = []
+fsa n a b c d = step1 ++ step2 ++ step3
+  where step1 = fsa k a c b d
+        step2 = hanoi (n-k) a b d
+        step3 = fsa k c b a d
+        k = n `div` 2
