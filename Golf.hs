001a37f7e27108ac246d810a214b4070a5071dab
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-05-30 15:05:43 +0200

    Homework 3 - Exercise 3

--- Golf.hs
+++ Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List
+
 -- Exercise 1
 
 everyNth :: [a] -> Int -> [a]
@@ -16,3 +18,13 @@ localMaxima _ = []
 
 -- Exercise 3
 
+frequencies :: [Integer] -> [(Integer, Int)]
+frequencies l = [ (i, length r) | (i:r) <- group $ sort $ l ++ [0..9] ]
+
+column :: Int -> (Integer, Int) -> String
+column m (i, n) = replicate (m-n) ' ' ++ replicate n '*' ++ "=" ++ show i
+
+histogram :: [Integer] -> String
+histogram l = unlines $ transpose $ map (column max) freqs
+  where max = maximum $ map snd freqs
+        freqs = frequencies l
