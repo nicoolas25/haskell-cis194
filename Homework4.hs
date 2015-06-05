7c6671632d53d1fe0c8096dfdd0aee768c35a194
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-06-05 11:38:14 +0200

    Homework 4 - Exercise 1

---
+++ Homework4.hs
@@ -0,0 +1,12 @@
+-- Exercise 1
+
+fun1 :: [Integer] -> Integer
+fun1 = product . map (subtract 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 = sum . filter even . takeWhile (> 1) . iterate serie
+  where serie n = if even n then n `div` 2 else 3 * n + 1
+
+-- Exercise 2
+
+
